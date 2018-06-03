d97ed1cc78f4af428c3167659e6c2eba8b1e6992
Jackiossy <jackiossy@gmail.com>
2018-06-03 22:11:46 +0800

    UI code init.

---
+++ Raccool/Raccool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Raccool
+//
+//  Created by jackiossy on 2018/6/3.
+//  Copyright © 2018 jackiossy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Raccool/Raccool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Raccool
+//
+//  Created by jackiossy on 2018/6/3.
+//  Copyright © 2018 jackiossy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
